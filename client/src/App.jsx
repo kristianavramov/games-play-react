19ddc952f08d2a962e3202d85d1f24db1990868d
kristianavramov <kavramov07@gmail.com>
2024-01-16 15:24:54 +0200

    transforming html to jsx components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,25 @@
+import Catalogue from "./components/Catalogue/Catalogue";
+import CreateGame from "./components/Create-game/Create-game";
+import GameDetails from "./components/Game-details/Game-details";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
 
 function App() {
-
-  return (
-    <>
-      <h1>Hello</h1>
-    </>
-  )
+    return (
+        <>
+            <div id="box">
+                <Header />
+                <Home />
+                <Login />
+                <Register />
+                <CreateGame/>
+                <GameDetails/>
+                <Catalogue />
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
