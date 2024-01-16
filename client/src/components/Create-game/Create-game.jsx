13f9fbce894ba73629ee503c0d619cc23057c837
kristianavramov <kavramov07@gmail.com>
2024-01-16 17:54:29 +0200

    crating game with controlled form and saving it with useState

--- client/src/components/Create-game/Create-game.jsx
+++ client/src/components/Create-game/Create-game.jsx
@@ -1,27 +1,106 @@
-export default function CreateGame(){
+import { useState } from "react";
+
+let initialData = {
+    title: "",
+    category: "",
+    maxLevel: "",
+    imgUrl: "",
+    summary: "",
+};
+
+export default function CreateGame() {
+    let [createdGame, setCreatedGame] = useState(initialData);
+    
     return (
         <section id="create-page" className="auth">
             <form id="create">
                 <div className="container">
-
                     <h1>Create Game</h1>
                     <label htmlFor="leg-title">Legendary title:</label>
-                    <input type="text" id="title" name="title" placeholder="Enter game title..."/>
+                    <input
+                        type="text"
+                        id="title"
+                        name="title"
+                        placeholder="Enter game title..."
+                        onChange={(e) =>
+                            setCreatedGame({
+                                ...createdGame,
+                                title: e.target.value,
+                            })
+                        }
+                        value={createdGame.title}
+                    />
 
                     <label htmlFor="category">Category:</label>
-                    <input type="text" id="category" name="category" placeholder="Enter game category..."/>
+                    <input
+                        type="text"
+                        id="category"
+                        name="category"
+                        placeholder="Enter game category..."
+                        onChange={(e) =>
+                            setCreatedGame({
+                                ...createdGame,
+                                category: e.target.value,
+                            })
+                        }
+                        value={createdGame.category}
+                    />
 
                     <label htmlFor="levels">MaxLevel:</label>
-                    <input type="number" id="maxLevel" name="maxLevel" min="1" placeholder="1"/>
+                    <input
+                        type="number"
+                        id="maxLevel"
+                        name="maxLevel"
+                        min="1"
+                        placeholder="1"
+                        onChange={(e) =>
+                            setCreatedGame({
+                                ...createdGame,
+                                maxLevel: e.target.value,
+                            })
+                        }
+                        value={createdGame.maxLevel}
+                    />
 
                     <label htmlFor="game-img">Image:</label>
-                    <input type="text" id="imageUrl" name="imageUrl" placeholder="Upload a photo..."/>
+                    <input
+                        type="text"
+                        id="imageUrl"
+                        name="imageUrl"
+                        placeholder="Upload a photo..."
+                        onChange={(e) =>
+                            setCreatedGame({
+                                ...createdGame,
+                                imgUrl: e.target.value,
+                            })
+                        }
+                        value={createdGame.imgUrl}
+                    />
 
                     <label htmlFor="summary">Summary:</label>
-                    <textarea name="summary" id="summary"></textarea>
-                    <input className="btn submit" type="submit" value="Create Game"/>
+                    <textarea
+                        name="summary"
+                        id="summary"
+                        onChange={(e) =>
+                            setCreatedGame({
+                                ...createdGame,
+                                summary: e.target.value,
+                            })
+                        }
+                        value={createdGame.summary}
+                    ></textarea>
+                    <input
+                        className="btn submit"
+                        type="submit"
+                        value="Create Game"
+                        onClick={(e) => {
+                            e.preventDefault();
+                            console.log(createdGame);
+                            setCreatedGame(initialData)
+                        }}
+                    />
                 </div>
             </form>
         </section>
-    )
+    );
 }
